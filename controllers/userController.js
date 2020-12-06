9586d30d7f635f8ea3525fea31eef920b10c344f
Dominik Swiatek <dominik.swiatek1995@gmail.com>
2020-12-06 15:24:43 +0100

    add login, logout and refresh token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,46 @@
 const Users = require('../models/user')
 const bcrypt = require('bcrypt')
 const { createAccesToken, createRefreshToken } = require('../helpers/createTokens')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
 exports.userController = {
 
-    login: function () {
-        console.log("login")
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body
+
+            const user = await Users.findOne({ email })
+            if (!user) return res.status(400).json({ msg: 'email or password are not correct' })
+
+            const comparePassword = await bcrypt.compare(password, user.password)
+            if (!comparePassword) return res.status(400).json({ msg: 'email or password are not correct' })
+
+            const accesToken = createAccesToken({ id: user.id })
+            const refreshToken = createRefreshToken({ id: user.id })
+
+            res.cookie('refreshtoken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refreshToken',
+                maxAge: 30 * 24 * 60 * 60 * 1000
+            })
+
+            res.json({ accesToken })
+
+
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
     },
 
-    register: async function (req, res) {
+    register: async (req, res) => {
         try {
             const { email, userName, password, repeatPassword } = req.body
 
             const user = await Users.findOne({ email })
-            if (user) throw new Error("User arleady exists")
+            if (user) return res.status(400).json({ msg: "User arleady exists" })
 
-            if (password !== repeatPassword) throw new Error("Passwords are not correct")
+            if (password !== repeatPassword) return res.status(400).json({ msg: "Passwords are not correct" })
             const hashedPassword = await bcrypt.hash(password, 10)
 
             const newUser = new Users({
@@ -30,7 +56,7 @@ exports.userController = {
 
             res.cookie('refreshtoken', refreshToken, {
                 httpOnly: true,
-                path: '/user/refresh_token',
+                path: '/user/refreshToken',
                 maxAge: 30 * 24 * 60 * 60 * 1000
             })
 
@@ -38,7 +64,36 @@ exports.userController = {
 
         }
         catch (error) {
-            console.log(error)
+            return res.status(500).json({ msg: error.message })
+        }
+    },
+
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', { path: '/user/refreshToken' })
+            return res.json({ msg: "Logged" })
+
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
+        }
+
+    },
+
+    refreshToken: (req, res) => {
+        try {
+            const refreshToken = req.cookies.refreshtoken
+            if (!refreshToken) return res.status(400).json({ msg: 'Please Login or Register' })
+
+            jwt.verify(refreshToken, process.env.SECRET_REFRESH_TOKEN, (error, user) => {
+                if (error) return res.status(400).json({ msg: 'Please Login or Register' })
+
+                const accesToken = createAccesToken({ id: user.id })
+
+                res.json({ accesToken })
+            })
+
+        } catch (error) {
+            return res.status(500).json({ msg: error.message })
         }
     }
 }
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router()
 const { userController } = require('../controllers/userController')
 
-router.get('/login', userController.login)
+router.post('/login', userController.login)
 
 router.post('/register', userController.register)
+
+router.post('/logout', userController.logout)
+
+router.get('/refreshToken', userController.refreshToken)
 
 module.exports = router
